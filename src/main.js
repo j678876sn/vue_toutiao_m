24ce3039f7915fbc8308f6e2c630b9d4f43a6ddb
孙龙 <843347654@qq.com>
2020-05-11 21:11:34 +0800

    引入vant组件库

--- src/main.js
+++ src/main.js
@@ -2,11 +2,19 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+// 引入vant组件
+import Vant from 'vant';
+import 'vant/lib/index.css';
 // 加载全局样式
 import './style/index.less'
 
+
 Vue.config.productionTip = false
+
+// 挂载vant组件库
+Vue.use(Vant);
+
+
 
 new Vue({
   router,
